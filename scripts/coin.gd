6658a5d186ea7346ede7bcf7555e967b554ea3c2
Bima Turangga <bimaturanggabayupratama@gmail.com>
2026-01-03 01:35:12 +0700

    update 1.1

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,8 +1,8 @@
 extends Area2D
 
+@onready var game_manager: Node = %gameManager
+@onready var pickup_animation: AnimationPlayer = $pickupAnimation
 
-
-
-func _on_body_entered(body):
-	print("+1 coin!")
-	queue_free()
+func _on_body_entered(body: Node2D) -> void:
+	game_manager.add_point()
+	pickup_animation.play("pickup")
